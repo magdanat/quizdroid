b3cb8367c58dd33305c738b3c673298d9e3489ef
magdanat <nathanmagdalera@gmail.com>
2019-04-27 22:08:35 -0700

    First commit

---
+++ app/src/main/java/edu/washington/magdanat/quizdroid/ListFragment.kt
@@ -0,0 +1,37 @@
+package edu.washington.magdanat.quizdroid
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class ListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment (needs to be returned later)
+        val inflate =  inflater.inflate(R.layout.fragment_list, container, false)
+
+//        inflate.findViewById<Button>(R.id.button).setOnClickListener {
+//            it.findNavController().navigate(R.id.action_listFragment_to_detailFragment)
+//        }
+
+        return inflate
+    }
+
+
+}
