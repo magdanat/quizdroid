b97b796bc4e7122fae7ddd0c9b5e37d0887e6dcc
magdanat <nathanmagdalera@gmail.com>
2019-04-28 01:44:48 -0700

    Add overview

--- app/src/main/java/edu/washington/magdanat/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/MainActivity.kt
@@ -1,12 +1,19 @@
 package edu.washington.magdanat.quizdroid
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TabHost
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabHost = findViewById<TabHost>(android.R.id.tabhost)
+        val tabSpec = tabHost.newTabSpec("tab1")
+        tabSpec.setIndicator(R.layout.acti)
+        val intent = Intent(this, )
     }
 }
