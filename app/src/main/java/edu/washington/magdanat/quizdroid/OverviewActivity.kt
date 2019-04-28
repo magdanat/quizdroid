a837268d4cba6234d013ff81f51fe4f019b90308
magdanat <nathanmagdalera@gmail.com>
2019-04-28 00:15:25 -0700

    Edit questionactivity.kt

--- app/src/main/java/edu/washington/magdanat/quizdroid/OverviewActivity.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/OverviewActivity.kt
@@ -1,2 +1,12 @@
 package edu.washington.magdanat.quizdroid
 
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OverviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/edu/washington/magdanat/quizdroid/QuestionActivity.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/QuestionActivity.kt
@@ -1,2 +1,12 @@
 package edu.washington.magdanat.quizdroid
 
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.question_activity)
+    }
+}
