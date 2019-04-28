3896a6facbb7887b191299651d9615dc72c5b02c
magdanat <nathanmagdalera@gmail.com>
2019-04-28 03:14:13 -0700

    .

--- app/src/main/java/edu/washington/magdanat/quizdroid/HomeActivity.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/HomeActivity.kt
@@ -3,20 +3,26 @@ package edu.washington.magdanat.quizdroid
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import kotlinx.android.synthetic.main.item.view.*
+
+import kotlinx.android.synthetic.main.home_activity.*
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.overview_activity)
+        setContentView(R.layout.home_activity)
+
+        // Hard-coded for now
+        val list = listOf("Math", "Physics", "Marvel Super Heroes")
+
+
+
+
+//
+//        val adapter = MyAdapter(list)
+//
+//        myRecyclerView.adapter = adapter
+//        myRecyclerView.setHasFixedSize(true)
+//        myRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 }
--- app/src/main/java/edu/washington/magdanat/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/MainActivity.kt
@@ -1,19 +1,21 @@
 package edu.washington.magdanat.quizdroid
 
+import android.app.TabActivity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.widget.TabHost
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : TabActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tabHost = findViewById<TabHost>(android.R.id.tabhost)
-        val tabSpec = tabHost.newTabSpec("tab1")
-        tabSpec.setIndicator(R.layout.overview_activity)
-        val intent = Intent(this, )
+//        val tabHost = findViewById<TabHost>(android.R.id.tabhost)
+//        val tabSpec = tabHost.newTabSpec("tab1")
+//        tabSpec.setIndicator(R.layout.overview_activity)
+//        val intent = Intent(this, )
     }
 }
