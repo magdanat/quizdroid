a45a850cd70b6bf6515b62ecb131bc732f1994c7
magdanat <nathanmagdalera@gmail.com>
2019-04-29 02:30:12 -0700

    Edit files

--- app/src/main/java/edu/washington/magdanat/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/MainActivity.kt
@@ -1,13 +1,9 @@
 package edu.washington.magdanat.quizdroid
 
-import android.app.TabActivity
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.widget.TabHost
 
-class MainActivity : TabActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
