c2f0b93088bc99fc33a020fe5b6d2a424141d452
magdanat <nathanmagdalera@gmail.com>
2019-04-29 16:55:36 -0700

    Update adapter

--- app/src/main/java/edu/washington/magdanat/quizdroid/ListFragment.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/ListFragment.kt
@@ -4,12 +4,13 @@ package edu.washington.magdanat.quizdroid
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_list.*
+
 //import android.widget.Button
 //import androidx.navigation.findNavController
 
@@ -20,45 +21,21 @@ class ListFragment : Fragment() {
 
     private var listener: OnListFragmentInteractionListener? = null
 
-    override fun onCreate(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
+    private lateinit var recycler : RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_list, container, false)
         val list = listOf("Eric","Test","Show")
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> android.support.v7.widget.LinearLayoutManager(context)
-                    else -> android.support.v7.widget.GridLayoutManager(context, columnCount)
-                }
-                adapter = RecyclerViewAdapter(list, listener)
-            }
-        }
+
+        val adapter = RecyclerViewAdapter(list, listener)
+        recycler = view.findViewById(R.id.myRecyclerView)
+        recycler.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        recycler.adapter = adapter
+//        recycler.setHasFixedSize(true)
         return view
     }
 
-//    override fun onAttach(context: Context) {
-//        super.onAttach(context)
-//        if (context is OnListFragmentInteractionListener) {
-//            listener = context
-//        } else {
-//            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
-//        }
-//    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
     interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
         fun onListFragmentInteraction(item: List<String>)
     }
 
@@ -76,4 +53,5 @@ class ListFragment : Fragment() {
                 }
             }
     }
+
 }
