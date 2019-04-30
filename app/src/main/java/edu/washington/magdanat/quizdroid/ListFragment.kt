80b564f750a6ecad4cd9badafe3c2d16d287394a
magdanat <nathanmagdalera@gmail.com>
2019-04-29 19:51:32 -0700

    Update fragment navigation

--- app/src/main/java/edu/washington/magdanat/quizdroid/ListFragment.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/ListFragment.kt
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_list.*
 
 //import android.widget.Button
@@ -19,39 +20,45 @@ class ListFragment : Fragment() {
 
     private var columnCount = 1
 
-    private var listener: OnListFragmentInteractionListener? = null
+//    private var listener: OnListFragmentInteractionListener? = null
 
     private lateinit var recycler : RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_list, container, false)
-        val list = listOf("Eric","Test","Show")
+        val list = listOf("Math","Physics","Marvel Super Heroes")
 
-        val adapter = RecyclerViewAdapter(list, listener)
+//        val adapter = RecyclerViewAdapter(list, listener)
+        val adapter = RecyclerViewAdapter(list)
         recycler = view.findViewById(R.id.myRecyclerView)
         recycler.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         recycler.adapter = adapter
-//        recycler.setHasFixedSize(true)
+        recycler.setHasFixedSize(true)
+
+//        adapter.onPersonClickedListener = { position, name ->
+//            Toast.makeText(context, "$name clicked!", Toast.LENGTH_SHORT).show()
+//        }
+
         return view
     }
 
-    interface OnListFragmentInteractionListener {
-        fun onListFragmentInteraction(item: List<String>)
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            ListFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
-    }
+//    interface OnListFragmentInteractionListener {
+//        fun onListFragmentInteraction(item: List<String>)
+//    }
+//
+//    companion object {
+//
+//        // TODO: Customize parameter argument names
+//        const val ARG_COLUMN_COUNT = "column-count"
+//
+//        // TODO: Customize parameter initialization
+//        @JvmStatic
+//        fun newInstance(columnCount: Int) =
+//            ListFragment().apply {
+//                arguments = Bundle().apply {
+//                    putInt(ARG_COLUMN_COUNT, columnCount)
+//                }
+//            }
+//    }
 
 }
--- app/src/main/java/edu/washington/magdanat/quizdroid/RecyclerViewAdapter.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/RecyclerViewAdapter.kt
@@ -5,32 +5,69 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.item.view.*
 
-import edu.washington.magdanat.quizdroid.ListFragment.OnListFragmentInteractionListener
+//import edu.washington.magdanat.quizdroid.ListFragment.OnListFragmentInteractionListener
 
 
-class RecyclerViewAdapter(private var list: List<String>, private var mListener: OnListFragmentInteractionListener?): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+//class RecyclerViewAdapter(private var list: List<String>, private var mListener: OnListFragmentInteractionListener?): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewHolderType: Int): ViewHolder {
+//        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+//        return ViewHolder(itemView)
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return list.size
+//    }
+//
+//    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+//            View.OnClickListener { v ->
+//            val item = v.tag as List<String>
+//            mListener?.onListFragmentInteraction(item)
+//        }
+//
+//    }
+//
+//    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//
+//    }
+//
+//}
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewHolderType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
-        return ViewHolder(itemView)
-    }
+ class RecyclerViewAdapter(var list: List<String>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    override fun getItemCount(): Int {
-        return list.size
-    }
+//     var onPersonClickedListener: ((position: Int, name: String) -> Unit)? = null
+//    var onPersonClickedListener: ((position: Int, name: String) -> Unit)? = null
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-            View.OnClickListener { v ->
-            val item = v.tag as List<String>
-            mListener?.onListFragmentInteraction(item)
+     override fun onCreateViewHolder(parent: ViewGroup, viewHolderType: Int): ViewHolder {
+         // Creates ViewHolder to hold reference of the views
+         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+         return ViewHolder(itemView)
+     }
+
+     override fun getItemCount(): Int {
+         return list.size
+     }
+
+     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+         viewHolder.bindView(list[position], position)
+     }
+
+
+     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindView(listItem: String, position: Int) {
+            itemView.content.text = listItem
+
+            itemView.setOnClickListener {
+//                onPersonClickedListener?.invoke(position, listItem)
+//                Toast.makeText(itemView.context,  itemView.content.text.toString() + "clicked!", Toast.LENGTH_SHORT).show()
+                it.findNavController().navigate(R.id.action_listFragment_to_detailFragment)
+            }
         }
 
-    }
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
-
 }
