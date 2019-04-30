a06dc35c5512fe0ac9046ac829b2c2cf6ca184d3
magdanat <nathanmagdalera@gmail.com>
2019-04-30 01:46:26 -0700

    Error fixing with merging manifest

---
+++ app/src/main/java/edu/washington/magdanat/quizdroid/Data.kt
@@ -0,0 +1,3 @@
+package edu.washington.magdanat.quizdroid
+
+class
--- app/src/main/java/edu/washington/magdanat/quizdroid/RecyclerViewAdapter.kt
+++ app/src/main/java/edu/washington/magdanat/quizdroid/RecyclerViewAdapter.kt
@@ -1,49 +1,40 @@
 package edu.washington.magdanat.quizdroid
 
+import android.content.Context
+import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.item.view.*
 
-//import edu.washington.magdanat.quizdroid.ListFragment.OnListFragmentInteractionListener
-
-
-//class RecyclerViewAdapter(private var list: List<String>, private var mListener: OnListFragmentInteractionListener?): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewHolderType: Int): ViewHolder {
-//        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
-//        return ViewHolder(itemView)
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return list.size
-//    }
-//
-//    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-//            View.OnClickListener { v ->
-//            val item = v.tag as List<String>
-//            mListener?.onListFragmentInteraction(item)
-//        }
-//
-//    }
-//
-//    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//
-//    }
-//
-//}
 
  class RecyclerViewAdapter(var list: List<String>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-//     var onPersonClickedListener: ((position: Int, name: String) -> Unit)? = null
-//    var onPersonClickedListener: ((position: Int, name: String) -> Unit)? = null
+     lateinit var variableChosen : Bundle
+
+     private val variableMath = bundleOf(
+         "Topic" to "Math",
+         "Description" to "This quiz will be a series of math questions",
+         "Questions" to "5"
+     )
+
+     private val variablePhysics = bundleOf(
+         "Topic" to "Physics",
+         "Description" to "This quiz will be a series of physics questions",
+         "Questions" to "2"
+     )
+
+     private val variableMCU = bundleOf(
+         "Topic" to "Marvel Super Heroes",
+         "Description" to "This quiz will be a series of questions about the Marvel Cinematic Universe",
+         "Questions" to "3"
+     )
 
      override fun onCreateViewHolder(parent: ViewGroup, viewHolderType: Int): ViewHolder {
-         // Creates ViewHolder to hold reference of the views
          val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
          return ViewHolder(itemView)
      }
@@ -56,18 +47,14 @@ import kotlinx.android.synthetic.main.item.view.*
          viewHolder.bindView(list[position], position)
      }
 
-
      inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindView(listItem: String, position: Int) {
             itemView.content.text = listItem
 
             itemView.setOnClickListener {
-//                onPersonClickedListener?.invoke(position, listItem)
-//                Toast.makeText(itemView.context,  itemView.content.text.toString() + "clicked!", Toast.LENGTH_SHORT).show()
-                it.findNavController().navigate(R.id.action_listFragment_to_detailFragment)
+                Toast.makeText(itemView.context,  itemView.content.text.toString() + "clicked!", Toast.LENGTH_SHORT).show()
+//                it.findNavController().navigate(R.id.action_listFragment_to_detailFragment, variableChosen)
             }
         }
-
-
     }
 }
